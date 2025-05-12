68f783961b9a1a4f10ecffdaf2e76d06748498b2
vikram jagtap <imvikramjagtap@gmail.com>
2025-05-12 21:10:05 +0530

    feat: extended GraphQL query in useGetPokemons to fetch name, number, types, and image

--- src/hooks/useGetPokemons.ts
+++ src/hooks/useGetPokemons.ts
@@ -16,10 +16,10 @@ export const GET_POKEMONS = gql`
   query pokemons($first: Int!) {
     pokemons(first: $first) {
       id
-      number
       name
-      image
+      number
       types
+      image
     }
   }
 `;
